63892ee7c858955dffe089e618d15f18bb24396e
unknown <jitendranathswamijsm@gmail.com>
2025-04-11 23:11:39 +0530

    simple 2nd comment

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 require("dotenv").config();
 const utils = require("./utils/utils.js");
 
-async function main() {
-  await utils.getCurrentMachineName();
+// 1. Get the current Machine's username
+const systemName = await utils.getCurrentMachineName();
+
+async function updateSystemSetup() {
+  // 1. Get all members of which do not have chrome profile the current system
+  /* Note- At present API to get "All members which do have chrome profile the current system" is not developed yet so till then let's give that array as variable. */
+  const members = [{}, {}, {}, {}];
 }
-main();
+updateSystemSetup();
+
+// C:\Program Files\Google\Chrome\Application\chrome.exe --profile-directory="Profile 2" --remote-debugging-port=9222 --window-size=1366,768 --window-position=0,0
